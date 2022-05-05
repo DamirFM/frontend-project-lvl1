17aa6ee8a26442e56db13dc0c8c7b32f4dfd4388
Damir Yakupov <yakupovdamir93@gmail.com>
2022-05-05 08:57:47 -0400

    add greetings string add cli.js / update package.json

---
+++ bin/brain-games.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line import/no-unresolved
+// eslint-disable-next-line no-unused-vars
+import { userName } from '../src/cli.js ';
+
+console.log('Welcome to the Brain Games!');
