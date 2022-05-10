67f28d0793f7df87fbf650f19797f2d9918c0f0b
Damir Yakupov <yakupovdamir93@gmail.com>
2022-05-10 08:56:06 -0400

    add brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-unresolved */
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line import/no-unresolved
+// eslint-disable-next-line no-unused-vars
+import { userName } from '../src/cli.js ';
+
+console.log('Welcome to the Brain Games!');
