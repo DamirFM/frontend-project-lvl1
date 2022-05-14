3084b8efd225757931fac92a7eb48e59f32d6116
Damir Yakupov <yakupovdamir93@gmail.com>
2022-05-13 20:11:49 -0400

    13/05/2022

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
+/* eslint-disable no-undef */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable no-unused-vars */
+
 import * as cliEvenJs from '../src/cli-even.js ';
+import { even } from '../src/cli-even.js';
 
 console.log(`Hi ${cliEvenJs.userName}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 console.log(`Question: ${cliEvenJs.randomNumber}`);
+console.log(even());
--- src/cli-even.js
+++ src/cli-even.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
@@ -5,3 +6,32 @@ console.log('Welcome to the Brain Games!');
 export const userName = readlineSync.question('May I have your name? ');
 
 export const randomNumber = Math.floor(Math.random() * 100);
+
+export const answer = readlineSync.question('Your answer: ');
+
+export const even = () => {
+  if (randomNumber % 2 === 0 && answer === 'yes' && answer !== 'no') {
+    return 'Correct!';
+  }
+  if (randomNumber % 2 === 0 && answer === 'no' && answer !== 'yes') {
+    return ` "yes" is wrong answer ;(. Correct answer was "no".
+    Let's try again, ${userName}!
+      `;
+  }
+  if (randomNumber % 2 !== 0 && answer === 'no' && answer !== 'yes') {
+    return 'Correct!';
+  }
+  if (randomNumber % 2 !== 0 && answer === 'yes' && answer !== 'no') {
+    return ` "yes" is wrong answer ;(. Correct answer was "no".
+      Let's try again, ${userName}!
+        `;
+  }
+  if (answer !== 'yes' && 'no') {
+    return 'ERROR';
+  }
+  for (let i = 0; i < 4; i += 1) {
+    if (even === 'Correct!') {
+      return even();
+    }
+  }
+};
