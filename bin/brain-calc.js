aced9fc708cf339e3f024875b4eff12c6e3f77eb
Damir Yakupov <yakupovdamir93@gmail.com>
2022-05-24 09:25:54 -0400

    update

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable import/extensions */
 
 import { logic } from '../src/games/cli-calc.js';
 
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,11 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/prefer-default-export */
 import readlineSync from 'readline-sync';
 // Wait for user's response.
 // eslint-disable-next-line import/prefer-default-export
-export const userName = readlineSync.question('May I have your name? ');
-console.log(`Hi ${userName}!`);
+export const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
