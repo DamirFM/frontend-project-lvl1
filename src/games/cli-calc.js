522adcf015711f7b66d47b460e269bd6a7945173
Damir Yakupov <yakupovdamir93@gmail.com>
2022-05-25 09:56:47 -0400

    25/05/22

--- src/games/cli-calc.js
+++ src/games/cli-calc.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable consistent-return */
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
 /* eslint-disable import/extensions */
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable no-shadow */
@@ -5,13 +9,32 @@ import { mainLogic } from '../index.js';
 
 const intro = 'What is the result of the expression?';
 
+const arithmetic = ['+', '-', '*'];
+
 const num1 = Math.floor(Math.random() * 100);
 const num2 = Math.floor(Math.random() * 100);
 
-const expression = (num1, num2) => [(num1) + (num2), (num1) - (num2), (num1) * (num2)];
-const core = () => {
-  const question = [`${num1} + ${num2}`, `${num1} - ${num2}`, `${num1} * ${num2}`];
-  const correctAnswer = expression(num1, num2) ? 'yes' : 'no';
-  return (question, correctAnswer);
+const expression = (num1, arithmetic, num2) => {
+  switch (arithmetic) {
+    case '+':
+      (num1) + (num2);
+      break;
+    case '-':
+      (num1) - (num2);
+      break;
+    case '*':
+      (num1) * (num2);
+      break;
+    default:
+      return null;
+  }
 };
+
+const core = () => {
+  const someArithmetic = arithmetic[Math.floor(Math.random(0, arithmetic.length - 1))];
+  const question = `${num1} ${someArithmetic} ${num2} ?`;
+  const correctAnswer = `${expression(num1, someArithmetic, num2)}`;
+  return [question, correctAnswer];
+};
+
 export const logic = () => mainLogic(intro, core);
