8af718713b6399608d1a50f99302db36372e4d07
Damir Yakupov <yakupovdamir93@gmail.com>
2022-05-29 23:55:09 -0400

    cli-prime

---
+++ src/games/cli-prime.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/extensions */
+/* eslint-disable no-unused-vars */
+import { mainLogic } from '../index.js';
+
+const intro = 'Answer "yes" if given number is prime. Otherwise answer "no".';
