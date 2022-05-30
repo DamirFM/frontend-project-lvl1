6f88d0dca8b044ea2623e5cf9629eff213c984c1
Damir Yakupov <yakupovdamir93@gmail.com>
2022-05-29 23:31:06 -0400

    even game update

--- src/games/cli-even.js
+++ src/games/cli-even.js
@@ -8,11 +8,11 @@ const intro = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => number % 2 === 0;
 
-const gameCheck = () => {
+const core = () => {
   const number = Math.floor(Math.random() * 100);
   const question = `${number}`;
   const correctAnswer = isEven(number) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
-export const logic = () => mainLogic(intro, gameCheck);
+export const logic = () => mainLogic(intro, core);
