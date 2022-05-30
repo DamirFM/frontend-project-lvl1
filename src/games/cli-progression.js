fe2e51283a9c22c1365c5ce4b098e3ce32c003af
Damir Yakupov <yakupovdamir93@gmail.com>
2022-05-29 23:05:12 -0400

    29/05/22

--- src/games/cli-progression.js
+++ src/games/cli-progression.js
@@ -9,27 +9,26 @@
 import { mainLogic } from '../index.js';
 
 const intro = 'What number is missing in the progression?';
-
-const progression = (firstItem, step, length) => {
-  const arr = [];
-  arr[0] = firstItem;
-  for (let i = 0; i < length; i += 1) {
-    const result = arr[i - 1] + step;
-    arr.push(result);
-    console.log(arr);
+const mainProgression = (firstItem, step, mainProgressionLength) => {
+  const array = [];
+  for (let i = 0; i < mainProgressionLength; i += 1) {
+    const result = (firstItem + (step * i));
+    array.push(result);
   }
-  return arr;
+  return array;
 };
 
 function core() {
-  const ranFirst = Math.floor(Math.random() * 100);
-  const ranStep = Math.floor(Math.random() * 10);
-  const ranLenght = Math.floor(Math.random() * 10);
-  const ranProgression = progression(ranFirst, ranStep, ranLenght);
-  const ranIndex = Math.floor(Math.random() * ranProgression.length);
-  const question = ranProgression.join(' ');
-  const correctAnswer = `${ranProgression[ranIndex]}`;
-  ranProgression[ranIndex] = '..';
+  const first = Math.floor(Math.random() * 10);
+  const step = Math.floor(Math.random() * 10);
+  const length = Math.floor(Math.random() * 10);
+  const progression = mainProgression(first, step, length);
+  const randIndex = Math.floor(Math.random() * progression.length);
+
+  const correctAnswer = `${progression[randIndex]}`;
+  progression[randIndex] = '..';
+  const question = progression.join(' ');
   return [question, correctAnswer];
 }
+
 export const logic = () => mainLogic(intro, core);
