b37e8f1f1614d581adbe0fc1c15a24df0c067c3a
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-13 19:49:42 -0400

    add cli-even.js

--- src/games/cli-even.js
+++ src/games/cli-even.js
@@ -1,15 +1,12 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable import/no-unresolved */
-/* eslint-disable import/extensions */
-import { mainLogic } from '../index.js';
+import mainLogic from '../index';
+import getRandomNumber from '../getRandomNumber';
 
 const intro = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const isEven = (number) => number % 2 === 0;
 
 export const core = () => {
-  const number = Math.floor(Math.random() * 100);
+  const number = getRandomNumber(1, 100);
   const question = `${number}`;
   const correctAnswer = isEven(number) ? 'yes' : 'no';
   return [question, correctAnswer];
