c4c41e4a847a0be5f338701ce5b96c2f9f8ac499
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-14 00:16:38 -0400

    add cli-prime.js

--- src/games/cli-prime.js
+++ src/games/cli-prime.js
@@ -3,19 +3,20 @@ import getRandomNumber from '../getRandomNumber';
 
 const intro = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-function core() {
-  const number = getRandomNumber(1, 100);
-  const isPrime = (num) => {
-    if (num < 2) {
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
       return false;
     }
-    for (let i = 2; i < num; i += 1) {
-      if (num % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  };
+  }
+  return true;
+};
+
+function core() {
+  const number = getRandomNumber(1, 100);
 
   const question = `${number}`;
 
