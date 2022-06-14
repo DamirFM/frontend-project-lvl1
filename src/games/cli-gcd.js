d146cd7cd6fedcc55000f41003b6f4f19b306fac
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-14 00:12:29 -0400

    add cli-gcd

--- src/games/cli-gcd.js
+++ src/games/cli-gcd.js
@@ -1,37 +1,23 @@
-/* eslint-disable import/extensions */
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable no-console */
-/* eslint-disable no-undef */
-/* eslint-disable no-self-assign */
-/* eslint-disable no-shadow */
-/* eslint-disable no-unused-expressions */
-/* eslint-disable no-plusplus */
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-unused-vars */
-import { mainLogic } from '../index.js';
+import mainLogic from '../index';
+import getRandomNumber from '../getRandomNumber';
 
 const intro = 'Find the greatest common divisor of given numbers.';
 
 const core = () => {
-  const num1 = Math.floor(Math.random() * 100);
-  const num2 = Math.floor(Math.random() * 100);
+  const num1 = getRandomNumber(1, 100);
+  const num2 = getRandomNumber(1, 100);
 
-  function fun(num1, num2) {
-    num1 > num2 || ([num1, num2] = [num1, num2]);
-    const length = num2 / 2;
-    let k = 1;
-    for (let i = 2; i <= length;) {
-      if (!(num2 % i) && !(num1 % i)) {
-        num2 /= i;
-        num1 /= i;
-        k *= i;
-      } else i++;
+  const gcd = (number1, number2) => {
+    if (number2 === 0) {
+      return number1;
     }
+    return gcd(number2, number1 % number2);
+  };
 
-    return k;
-  }
   const question = `${num1} ${num2} ?`;
-  const correctAnswer = `${fun(num1, num2)}`;
+  const correctAnswer = `${gcd(num1, num2)}`;
   return [question, correctAnswer];
 };
-export const logic = () => mainLogic(intro, core);
+const logic = () => mainLogic(intro, core);
+
+export default logic;
