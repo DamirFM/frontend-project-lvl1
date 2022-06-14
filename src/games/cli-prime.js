a4b6603c495dac96a7fbf6bc717cd0a9312083d3
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-13 23:38:06 -0400

    add cli-prime.js

--- src/games/cli-prime.js
+++ src/games/cli-prime.js
@@ -1,34 +1,29 @@
-/* eslint-disable consistent-return */
-/* eslint-disable no-unreachable-loop */
-/* eslint-disable no-shadow */
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable no-unreachable */
-/* eslint-disable no-plusplus */
-/* eslint-disable eqeqeq */
-/* eslint-disable import/extensions */
-/* eslint-disable no-unused-vars */
-import { mainLogic } from '../index.js';
+import mainLogic from '../index';
+import getRandomNumber from '../getRandomNumber';
 
 const intro = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function core() {
-  const num = Math.floor(Math.random() * 100);
+  const number = getRandomNumber(1, 100);
   const isPrime = (num) => {
     if (num < 2) {
       return false;
     }
-    for (let i = 2; i < num; i++) {
+    for (let i = 2; i < num; i += 1) {
       if (num % i === 0) {
         return false;
       }
     }
     return true;
   };
-  const question = `${num}`;
 
-  const correctAnswer = isPrime(num) ? 'yes' : 'no';
+  const question = `${number}`;
+
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
 
   return [question, correctAnswer];
 }
 
-export const logic = () => mainLogic(intro, core);
+const logic = () => mainLogic(intro, core);
+
+export default logic;
