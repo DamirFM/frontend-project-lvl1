246f502ff17b5afa7289d9ba7d8f952580e71b41
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-13 23:38:42 -0400

    add cli-progression.js

--- src/games/cli-progression.js
+++ src/games/cli-progression.js
@@ -1,12 +1,5 @@
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable no-empty */
-/* eslint-disable no-undef */
-/* eslint-disable import/extensions */
-/* eslint-disable no-unused-vars */
-/* eslint-disable consistent-return */
-/* eslint-disable no-unreachable-loop */
-
-import { mainLogic } from '../index.js';
+import mainLogic from '../index';
+import getRandomNumber from '../getRandomNumber';
 
 const intro = 'What number is missing in the progression?';
 const mainProgression = (firstItem, step, mainProgressionLength) => {
@@ -19,9 +12,9 @@ const mainProgression = (firstItem, step, mainProgressionLength) => {
 };
 
 function core() {
-  const first = Math.floor(Math.random() * 10);
-  const step = Math.floor(Math.random() * 10);
-  const length = Math.floor(5, Math.random() * 10);
+  const first = getRandomNumber(1, 10);
+  const step = getRandomNumber(1, 10);
+  const length = getRandomNumber(1, 10);
   const progression = mainProgression(first, step, length);
   const randIndex = Math.floor(Math.random() * progression.length);
 
@@ -31,4 +24,6 @@ function core() {
   return [question, correctAnswer];
 }
 
-export const logic = () => mainLogic(intro, core);
+const logic = () => mainLogic(intro, core);
+
+export default logic;
