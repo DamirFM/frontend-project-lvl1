a3dc3ad1325f050ebf44b73ceedc01328e2964e9
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-19 23:22:47 -0400

    add cli-even

--- src/games/cli-even.js
+++ src/games/cli-even.js
@@ -6,7 +6,7 @@ const intro = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const core = () => {
-  const num = getRandomNumber(1, 100);
+  const num = getRandomNumber(1, 99);
   const question = `${num}`;
   const correctAnswer = isEven(num) ? 'yes' : 'no';
   return [question, correctAnswer];
