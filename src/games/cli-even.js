dccee4e5ddc807d41507f2535afc80910323c544
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-19 23:17:38 -0400

    add cli-even

--- src/games/cli-even.js
+++ src/games/cli-even.js
@@ -3,12 +3,12 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const intro = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (num) => num % 2 === 0;
+const isEven = (number) => number % 2 === 0;
 
 const core = () => {
-  const number = getRandomNumber(1, 100);
-  const question = `${number}`;
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  const num = getRandomNumber(1, 100);
+  const question = `${num}`;
+  const correctAnswer = isEven(num) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
