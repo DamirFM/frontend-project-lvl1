99762ddc22f7004c1dc550b4931688552a763b1a
Damir Yakupov <yakupovdamir93@gmail.com>
2022-06-24 10:00:00 -0400

    add cli-gcd.js

--- src/games/cli-gcd.js
+++ src/games/cli-gcd.js
@@ -1,23 +1,17 @@
 import gameEngine from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
+import gcd from '../gcd.js';
 
-const intro = 'Find the greatest common divisor of given numbers.';
+const gameRules = 'Find the greatest common divisor of given numbers.';
 
 const generateRoundData = () => {
   const num1 = getRandomNumber(1, 100);
   const num2 = getRandomNumber(1, 100);
 
-  const gcd = (number1, number2) => {
-    if (number2 === 0) {
-      return number1;
-    }
-    return gcd(number2, number1 % number2);
-  };
-
-  const question = `${num1} ${num2} ?`;
+  const question = `${num1} ${num2}`;
   const correctAnswer = `${gcd(num1, num2)}`;
   return [question, correctAnswer];
 };
-const gcdGame = () => gameEngine(intro, generateRoundData);
+const gcdGame = () => gameEngine(gameRules, generateRoundData);
 
 export default gcdGame;
