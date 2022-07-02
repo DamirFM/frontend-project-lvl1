d9d6557e27376f6c4888b493110b49b1fda0e2a6
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-01 23:26:08 -0400

    add cli

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,8 @@
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/prefer-default-export */
 import readlineSync from 'readline-sync';
-// Wait for user's response.
-// eslint-disable-next-line import/prefer-default-export
-export const greetings = () => {
+
+const greetings = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
+export default greetings;
