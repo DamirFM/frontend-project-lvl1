b53f01ac4a08c66f30bf004f3659dba82b006324
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-05 22:27:14 -0400

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import readlineSync from 'readline-sync';
 
+const roundsCount = 3;
+
 const gameEngine = (gameRules, generateRoundData) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(gameRules);
-  const roundsCount = 3;
+  
   for (let i = 0; i !== roundsCount; i += 1) {
     const [question, correctAnswer] = generateRoundData();
     console.log(`Question: ${question}`);
