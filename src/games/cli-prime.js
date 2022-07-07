cd6e4db30c94739ffbe4e7dc7f7abcce6cbbcdb2
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-06 22:19:40 -0400

    add cli-prime

--- src/games/cli-prime.js
+++ src/games/cli-prime.js
@@ -7,7 +7,7 @@ const isPrime = (num) => {
   if (num < 2) {
     return false;
   }
-  for (let i = 2; i < Math.sqrt(num); i += 1) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return false;
     }
